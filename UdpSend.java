972a8585d32932c7350b935036c889bb8961f7fc
wadadaCN <xie_kun@163.com>
2018-12-06 22:53:28 +0800

    传输json数据

--- UdpSend.java
+++ UdpSend.java
@@ -1,17 +1,23 @@
 import java.net.*;
-public class UdpSend{
+public class UdpSender{
     public static void main(String[] args) throws Exception{
         DatagramSocket ds = new DatagramSocket();
-        String str = "000000000000000000000";
+        String str = "{" +
+                "\t\"DeviceMac\":\"123456789012345\",\n" +
+                "\t\"DeviceInfo\":\"Normal\",\n" +
+                "\t\"GPS_N\":\"123.45\",\n" +
+                "\t\"GPS_E\":\"123.45\",\n" +
+                "\t\"Warn_Sta\":\"1\"\n" +
+                "}";
         byte[] sendData = str.getBytes();
         //String ipAddress = InetAddress.getLocalHost().getHostAddress();
         String ipAddress = "127.0.0.1";
         int count = 0;
-        while (count < 200){
-            count++;
-           Thread.sleep(500);
-            ds.send(new DatagramPacket(sendData, sendData.length, InetAddress.getByName(ipAddress), 3000));
-        }
+//        while (count < 200){
+//            count++;
+//            Thread.sleep(500);
+//            ds.send(new DatagramPacket(sendData, sendData.length, InetAddress.getByName(ipAddress), 3000));
+//        }
         ds.send(new DatagramPacket(sendData, sendData.length, InetAddress.getByName(ipAddress), 3000));
         ds.close();
     }
