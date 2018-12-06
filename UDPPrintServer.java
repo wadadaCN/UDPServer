1d780520f0cc1a5a3e4f8bb742566f9b6fe4cf49
wadadaCN <xie_kun@163.com>
2018-12-06 22:52:47 +0800

    解析json数据，写入数据库

--- UDPPrintServer.java
+++ UDPPrintServer.java
@@ -1,4 +1,6 @@
 import java.net.*;
+import java.sql.*;
+import com.google.gson.Gson;
 
 public class UDPPrintServer extends UDPServer{
     public final static int DEFAULT_PORT = 3000;
@@ -9,7 +11,35 @@ public class UDPPrintServer extends UDPServer{
 
     @Override
     public void response(DatagramSocket socket, DatagramPacket request){
-        System.out.println(new String(request.getData(), request.getOffset(), request.getLength()) + request.getAddress().toString() + ":" + request.getPort());
+        String jsonData = new String(request.getData(), request.getOffset(), request.getLength());
+        Gson gson = new Gson();
+        GPSdata gpsdata = gson.fromJson(jsonData, GPSdata.class);
+        String DeviceMac = gpsdata.DeviceMac;
+        String DeviceInfo = gpsdata.DeviceInfo;
+        double GPS_N = gpsdata.GPS_N;
+        double GPS_E = gpsdata.GPS_E;
+        int Warn_Sta = gpsdata.Warn_Sta;
+        Connection sqlConn = MySQLConnect.getConnection();
+        Statement stat = null;
+        try {
+            stat = sqlConn.createStatement();
+        }
+        catch (SQLException e){
+            System.out.println(e.getMessage());
+        }
+        String sql = "INSERT INTO alertServer" +
+                "(DeviceMac, DeviceInfo, GPS_N, GPS_E, Warn_Sta)" +
+                "VALUES" +
+                "(\"" + DeviceMac + "\", \"" + DeviceInfo + "\", \"" + GPS_N + "\", \"" + GPS_E +  "\", \"" + Warn_Sta + "\")";
+        if (stat != null){
+            try {
+                stat.executeUpdate(sql);
+            }catch (SQLException e){
+                System.out.println(e.getMessage());
+            }
+        }
+
+        //System.out.println(new String(request.getData(), request.getOffset(), request.getLength()) + request.getAddress().toString() + ":" + request.getPort());
     }
 
     public static void main(String[] args){
@@ -17,4 +47,12 @@ public class UDPPrintServer extends UDPServer{
         Thread t = new Thread(server);
         t.start();
     }
+}
+
+class GPSdata{
+    String DeviceMac;
+    String DeviceInfo;
+    double GPS_N;
+    double GPS_E;
+    int Warn_Sta;
 }
